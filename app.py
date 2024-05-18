258c4fb0c677db668aaf50fae03ab320367ee239
fajrin1996 <rinsyarifuddin@gmail.com>
2024-05-19 00:03:30 +0900

    membuat database

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 
 
 db = SQLAlchemy()
+
 class Fakultas(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     namaFak = db.Column(db.String(20), unique=True, nullable=False)
@@ -17,29 +18,46 @@ class Prodi(db.Model):
     fakulId = db.Column(db.Integer, db.ForeignKey('fakultas.id'), nullable=False)
     namaMHS = db.relationship('namaMahasiswa', backref='nama', lazy=True)
 
+class Panitia(db.Model):
+    nama = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(20), nullable=False)
+
 class namaMahasiswa(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
     npm = db.Column(db.String(40), nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
     posts = db.relationship('Post', backref='author', lazy=True)
+    syarat = db.relationship('syaratWis', backref='wisuda', lazy=True)
     prodId = db.Column(db.Integer, db.ForeignKey('prodi.id'), nullable=False)
     
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
 
-class Post(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    content = db.Column(db.Text, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+class syaratWis(db.Model):
+    bebas_ak = db.Column(db.String(50), nullable=False)
+    ijazah = db.Column(db.String(50), nullable=False)
+    pasfoto = db.Column(db.String(50), nullable=False)
+    karker = db.Column(db.String(50), nullable=False)
+    ktp = db.Column(db.String(50), nullable=False)
+    transnli = db.Column(db.String(50), nullable=False)
+    bebaspp = db.Column(db.String(50), nullable=False)
+    bbspus = db.Column(db.String(50), nullable=False)
+    sfkpp = db.Column(db.String(50), nullable=False)
+    mahasiswaId = db.Column(db.Integer, db.ForeignKey('namamahasiswa.id'), nullable=False)
 
-    def __repr__(self):
-        return f"Post('{self.title}', '{self.date_posted}')"
-
+# class Post(db.Model):
+    # id = db.Column(db.Integer, primary_key=True)
+    # title = db.Column(db.String(100), nullable=False)
+    # date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    # content = db.Column(db.Text, nullable=False)
+    # user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+# 
+    # def __repr__(self):
+        # return f"Post('{self.title}', '{self.date_posted}')"
+# 
 
 @app.route('/')
 def home():
